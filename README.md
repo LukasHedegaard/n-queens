815f5c0d45fdca91ec40d4dd3b3928c810b746dc
LukasHedegaard <lh@eng.au.dk>
2021-07-28 12:11:38 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# N-queens solver
+
+This repo contains a [N-queens problem](https://en.wikipedia.org/wiki/Eight_queens_puzzle) solver which uses recursion and [backtracking](https://en.wikipedia.org/wiki/Backtracking) in its implementation.
+
+Example solutions for the 4-queens problem:
+![Solutions to the 4-queens problem](https://assets.leetcode.com/uploads/2020/11/13/queens.jpg)
+
+## Usage
+1. [Install Rust](https://www.rust-lang.org/tools/install).
+1. Run example with `cargo run <n>`, where `<n>` is the board-size.
+
+```bash
+$ cargo run 4
+
+2 solution were found for the 4-queens problem:
+
+Solution 1
+".Q.."
+"...Q"
+"Q..."
+"..Q."
+
+Solution 2
+"..Q."
+"Q..."
+"...Q"
+".Q.."
+```
+
+```bash
+$ cargo run 8
+
+92 solution were found for the 4-queens problem:
+
+Solution 1
+"Q......."
+"....Q..."
+".......Q"
+".....Q.."
+"..Q....."
+"......Q."
+".Q......"
+"...Q...."
+
+...
+```
+
+## Performance
+
+Queens  | Soluions  | Time
+---     | ---       | --- 
+4       | 2         | 0.1 ms
+5       | 10        | 0.6 ms 
+6       | 4         | 2.4 ms
+7       | 40        | 10.0 ms
+8       | 92        | 45.8 ms
+9       | 352       | 227.2 ms
+10      | 724       | 1.1 s
+11      | 2680      | 6.1 s
+12      | 14200     | 36.2 s
---
+++ src/main.rs
@@ -0,0 +1,134 @@
+use std::env;
+use std::time::Instant;
+
+// We'll take a recuesive approach with backtracking
+struct Solution {}
+
+impl Solution {
+    pub fn solve_n_queens(n: i32) -> Vec<Vec<String>> {
+        let n = n as usize;
+        let mut sols = Vec::new();
+        let mut board = vec![vec!['_'; n]; n];
+
+        Solution::run(&mut board, &mut sols, 0, 0);
+
+        return sols;
+    }
+
+    fn run(
+        board: &mut Vec<Vec<char>>,
+        sols: &mut Vec<Vec<String>>,
+        start_row: usize,
+        start_col: usize,
+    ) {
+        for row in start_row..board.len() {
+            for col in start_col..board.len() {
+                if board[row][col] == '_' {
+                    for &val in ['Q', '.'].iter() {
+                        if Solution::valid(val, board, row, col) {
+                            // Try
+                            board[row][col] = val;
+                            Solution::run(board, sols, row, 0);
+
+                            // Backtrack
+                            board[row][col] = '_';
+                        }
+                    }
+                    return;
+                }
+            }
+        }
+        let mut sol = Vec::with_capacity(board.len());
+        for row in 0..board.len() {
+            sol.push(board[row].iter().collect());
+        }
+        sols.push(sol);
+    }
+
+    fn valid(val: char, board: &Vec<Vec<char>>, row: usize, col: usize) -> bool {
+        if val == 'Q' {
+            return Solution::valid_queen(board, row, col);
+        } else {
+            return Solution::valid_space(board, row, col);
+        }
+    }
+
+    fn valid_queen(board: &Vec<Vec<char>>, row: usize, col: usize) -> bool {
+        // Check row
+        for &c in &board[row] {
+            if c == 'Q' {
+                return false;
+            }
+        }
+
+        // Check col
+        for r in board {
+            if r[col] == 'Q' {
+                return false;
+            }
+        }
+
+        // Check diags
+        let mut j;
+        for (i, r) in board.iter().enumerate() {
+            j = (i as i32 - row as i32).abs() as usize;
+            if i != row {
+                if col + j < board.len() && r[col + j] == 'Q' {
+                    return false;
+                }
+                if col >= j && r[col - j] == 'Q' {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    fn valid_space(board: &Vec<Vec<char>>, row: usize, col: usize) -> bool {
+        let mut x = 0;
+        // Check row
+        for (i, &c) in board[row].iter().enumerate() {
+            if i != col && (c == 'Q' || c == '_') {
+                x += 1;
+                break;
+            }
+        }
+
+        // Check col
+        for (i, r) in board.iter().enumerate() {
+            if i != row && (r[col] == 'Q' || r[col] == '_') {
+                x += 1;
+                break;
+            }
+        }
+
+        return x == 2;
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    assert!(
+        args.len() == 2,
+        "Only a single argument should be given, which contains the board-size."
+    );
+    let n: i32 = args[1].parse::<i32>().unwrap();
+
+    let now = Instant::now();
+    let sols: Vec<Vec<String>> = Solution::solve_n_queens(n);
+
+    println!(
+        "{} solution were found for the {}-queens problem in {} ms:",
+        sols.len(),
+        n,
+        now.elapsed().as_millis()
+    );
+
+    for (i, sol) in sols.iter().enumerate() {
+        println!("\nSolution {}", i + 1);
+        for row in sol.iter() {
+            println!("{:?}", row);
+        }
+    }
+}
